7a52d5e4d48136cf72a34806d093d035d713bd06
Ender <sterlingkooshesh@gmail.com>
2022-07-09 21:14:30 -0500

    SCAFFOLDER doesn't add to git

---
+++ src/game/scenes/HomeKitchen/sprites/Jocounter/events/JackyTightButtHole.js
@@ -0,0 +1,10 @@
+import SceneSpriteEvents from "@/game/BaseClasses/SceneSpriteEvents";
+
+export const exportClass = class _JocounterJackyTightButtHoleDefaultSceneEvents extends SceneSpriteEvents {
+Started(Scene, Sprite, Payload) {
+    super.Started(Scene, Sprite, Payload);
+        debugger
+    Sprite.visible= true;
+}
+}
+export default exportClass
---
+++ src/game/scenes/SchoolGroundFloor/textboxes/TbMc/TbMc.js
@@ -0,0 +1,11 @@
+import {TbMcTextBox} from "./TbMcTextBox";
+
+    export const Name = "TbMc"
+    export const Id = "1"
+export const X = 600;
+export const Y =600;
+    export const FontFamily = 'Georgia, "Goudy Bookletter 1911", Times, serif'
+    export const Interactive = false;
+    export const EventsData = {}
+    export const TextClass = TbMcTextBox;
+

a9d86f8e035a5fca016371fdacaf4897f4ffc825
Ender <sterlingkooshesh@gmail.com>
2022-07-10 12:58:34 -0500

    Added Scaffolding CreateSprite AddFramesToSprite CreateTextBox CreateQuest Added documentation

--- src/game/scenes/TitleCArd/sprites/Paws/events/Paws.js
+++ src/game/scenes/TitleCArd/sprites/Paws/events/Paws.js
@@ -4,7 +4,7 @@ import {NewGame, SetSetting} from "@/store";
 export const exportClass = class _PawsDefaultSceneEvents extends SceneSpriteEvents {
     PointerUp(Scene, Sprite, Payload) {
         super.PointerUp(Scene, Sprite, Payload);
-        NewGame().then(()=>{
+        NewGame(Scene).then(()=>{
             scene.triggerSceneChange("preload_HomeKitchen")
         })
 
